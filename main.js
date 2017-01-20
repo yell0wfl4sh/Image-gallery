3964d2e5e6f4eaa9676ea7c00a4383759cf749b3
Yash Agrawal <yagrawal900@gmail.com>
2017-01-20 05:53:49 +0530

    without styling

---
+++ main.js
@@ -0,0 +1,36 @@
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
+
+btn = document.querySelector('button');
+var overlay = document.querySelector('.dislay');
+
+
+
+for(var i = 1; i <= 7; i++) {
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', 'img/img' + i + '.jpg');
+  thumbBar.appendChild(newImage);
+  newImage.onclick = function(e) {
+    var imgSrc = e.target.getAttribute('src');
+    displayImage(imgSrc);
+  }
+}
+
+function displayImage(imgSrc) {
+  displayedImage.setAttribute('src', imgSrc);
+}
+
+
+
+btn.onclick = function() {
+  var btnClass = btn.getAttribute('class'); 
+  if(btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+}
