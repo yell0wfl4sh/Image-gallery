e30b0441479547d93a67169889fa8d23ceaf510d
Yash Agrawal <yagrawal900@gmail.com>
2017-01-21 19:52:59 +0530

    added navigation button

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ var displayedImage = document.querySelector('.displayed-img');
 var thumbBar = document.querySelector('.thumb-bar');
 
 
-btn = document.querySelector('button');
+opacitybtn = document.getElementById('opac');
+upbtn = document.getElementById('updir');
+downbtn = document.getElementById('downdir');
 var overlay = document.querySelector('.display');
 
-
+var images=[];
+for(var i=1;i <=11;i++){
+  images.push('img/img'+ i+ '.jpg');
+}
 
 for(var i = 1; i <= 11; i++) {
   var newImage = document.createElement('img');
@@ -17,30 +22,52 @@ for(var i = 1; i <= 11; i++) {
   }
 }
 
-document.onkeydown = function(e){
+/*
+.onkeydown = function(e){
     e = e || window.event;
     var key = e.which || e.keyCode;
     if(key===84){
-        example();
-    }
-}
-
+        var imgSrc1 = e.target.('img/img1.jpg');
+        displayImage(imgSrc1);
+    }}
+*/
 function displayImage(imgSrc) {
   displayedImage.setAttribute('src', imgSrc);
 }
 
 
 
-btn.onclick = function() {
-  var btnClass = btn.getAttribute('class'); 
+opacitybtn.onclick = function() {
+  var btnClass = opacitybtn.getAttribute('class'); 
   if(btnClass === 'dark') {
-    btn.setAttribute('class','light');
-    btn.textContent = 'Lighten';
+    opacitybtn.setAttribute('class','light');
+    opacitybtn.textContent = 'Lighten';
     displayedImage.style.opacity = '0.5';
   } else {
-    btn.setAttribute('class','dark');
-    btn.textContent = 'Darken';
+    opacitybtn.setAttribute('class','dark');
+    opacitybtn.textContent = 'Darken';
     displayedImage.style.opacity = '1';
   }
 }
 
+upbtn.onclick=function(){
+  var current = displayedImage.getAttribute('src');
+  var index = images.indexOf(current)- 1;
+  if (index== -1){
+    displayImage(images[10]);
+  }
+else{
+  displayImage(images[index]);
+}
+}
+
+downbtn.onclick=function(){
+  var current = displayedImage.getAttribute('src');
+  var index = images.indexOf(current)+ 1;
+  if (index== 11){
+    displayImage(images[0]);
+  }
+else{
+  displayImage(images[index]);
+}
+}
